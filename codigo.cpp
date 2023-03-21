8a8dc7d6d1a3a70394c288bc3812723eca2fb713
Renee Birenbaum <dani.b.g.castro@gmail.com>
2023-03-20 21:09:17 -0300

    Version2

---
+++ codigo.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << "Ola, esta tudo funcionando!" << endl;
+    cout << "Esse codigo foi para o github atraves de comandos git!" << endl ;
+
+    if(1 >= 2){
+        cout << "Parece que 1 e maior que 2, ainda bem!" << endl;
+    }
+    for (int i{0}; i < 10; i++){
+        cout << "\nTudo indo bem pela: ";
+        cout << i << endl;
+    }
+    return 0;
+}
